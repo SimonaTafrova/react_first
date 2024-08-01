973f039cf7d43453ac7a072580030499ce5d5ef6
Simona Tafrova <simonatafrova@gmail.com>
2024-08-01 13:56:38 +0300

    Added a link to sing up page to already registered users

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import { images } from '../../constants'
 import FormField from '../components/FormField'
 import CustomButton from '../components/CustomButton'
+import { Link } from 'expo-router'
 
 
 const SignIn = () => {
@@ -17,7 +18,7 @@ const SignIn = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-center h-full px-4 my-6">
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
           <Image source={images.logo} resizeMode='contain' className="w-[115px] h-[35px]"/>
           <Text className="text-2xl text-white text-semibold mt-10 font-psemibold"> Log into Aura</Text>
           <FormField
@@ -40,8 +41,15 @@ const SignIn = () => {
           <CustomButton
           title="Sign-in"
           handlePress={submit}
-          containerStyles={mt-7}
+          containerStyles="mt-7"
           isLoading={isSubmitting} />
+
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">Don't have an account?</Text>
+            <Link href="/sign-up" className='text-lg font-psemibold text-secondary-100'>Sign Up</Link>
+          
+
+          </View>
 
           
 
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -28,6 +28,8 @@ const RootLayout = () => {
   return (
     <Stack>
         <Stack.Screen name="index" options={{headerShown: false}} />
+       
+    
     </Stack>
   )
 }
