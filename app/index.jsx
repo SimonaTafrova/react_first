5ebd371c65f42b060b70defc3f5dd16ae65ed87b
Simona Tafrova <simonatafrova@gmail.com>
2024-08-06 09:25:41 +0300

    Created Globalrovider.js

--- app/index.jsx
+++ app/index.jsx
@@ -4,9 +4,12 @@ import { Redirect, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../constants';
 import CustomButton from '../components/CustomButton'
+import { useGlobalContext } from '../context/GlobalProvider';
 
 
 export default function App() {
+    
+
   return (
     <SafeAreaView className="bg-primary h-full">
         <ScrollView contentContainerStyle={{ height: '100%'}}>
