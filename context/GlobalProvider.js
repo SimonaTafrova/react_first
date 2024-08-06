8e664e8ab67298b3e4c05bbb0020f2bd26b918df
Simona Tafrova <simonatafrova@gmail.com>
2024-08-06 09:20:23 +0300

    Created GlobalProvider.js

--- context/GlobalProvider.js
+++ context/GlobalProvider.js
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState, useEffect, Children } from "react";
+import { getCurrentUser } from "../lib/appwrite";
 
 const GlobalContext = createContext();
 export const useGlobalContext = () => useContext(GlobalContext);
@@ -7,9 +8,33 @@ const GlobalProvider = ({children }) => {
     const [isLoggedIn, setisLoggedIn] = useState(false)
     const [user, setUser] = useState(null)
     const [isLoading, setisLoading] = useState(true)
+
+    useEffect(() => {
+        getCurrentUser()
+        .then((res) => {
+            if(res){
+                setisLoggedIn(true);
+                setUser(res);
+            } else {
+                setisLoggedIn(false);
+                setUser(null);
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+        .finally(() => {
+            setisLoading(false);
+        })
+    })
     return (
         <GlobalContext.Provider
         value = {{
+            isLoggedIn,
+            setisLoggedIn,
+            user,
+            setUser,
+            isLoading
                 
         }}
         >
@@ -18,3 +43,5 @@ const GlobalProvider = ({children }) => {
         </GlobalContext.Provider>
     )
 }
+
+export default GlobalProvider;
