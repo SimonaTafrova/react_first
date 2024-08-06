3829b67d00d5e4520865835e35f42441f6e1243d
Simona Tafrova <simonatafrova@gmail.com>
2024-08-06 09:27:15 +0300

    Started designing the layout of home.jsx

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -5,7 +5,39 @@ import { images } from '../../constants'
 
 const Home = () => {
   return (
-    <View></View>
+    <SafeAreaView className="bg-primary">
+      <FlatList
+      data = {[{id:1}, {id:2}, {id:3}]}
+      keyExtractor={(item) => item.$id}
+      renderItem={({item}) => (
+        <Text className = "text-3xl">{item.id}</Text>
+      )}
+      ListHeaderComponent={() => (
+        <View className="my-6 px-4 space-y-6">
+          <View className="justify-between items-start flex-row mb-6">
+            <View className="">
+              <Text className="font-pmedium text-sm text-gray-100">
+                Welcome back
+              </Text>
+              <Text className="text-2xl font-psemibold text-white">
+                Simona!
+              </Text>
+
+            </View>
+            <View className="mt-1.5">
+              <Image  
+              source = {images.logoSmall}
+              className="w-9 h-10"
+              resizeMode='contain'/>
+
+            </View>
+
+          </View>
+
+        </View>
+      )}
+      />
+    </SafeAreaView>
     
   )
 }
