028fef50839373c4a65c7e147a7dac7af38c9523
Simona Tafrova <simonatafrova@gmail.com>
2024-08-24 21:51:07 +0300

    Started getting data from appwrite db in insuling tab

--- app/(tabs)/insulin.jsx
+++ app/(tabs)/insulin.jsx
@@ -1,11 +1,27 @@
-import { Text, View, FlatList } from 'react-native';
+import { Text, View, FlatList, RefreshControl } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useGlobalContext } from "../../context/GlobalProvider";
 import EmptyState from '../../components/EmptyState';
 import InfoBox from '../../components/InfoBox';
+import {getAllInsulinLogs} from '../../lib/appwrite'
+import useAppwrite from "../../lib/useAppwrite";
+import { useState } from 'react';
 
 const Insulin = () => {
+  
+  const { data: posts, refetch, error } = useAppwrite(getAllInsulinLogs);
+  const [refreshing, setRefreshing] = useState(false);
   const { user } = useGlobalContext();
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    try {
+      await refetch();
+    } catch (err) {
+      console.error('Failed to refresh posts:', err);
+    }
+    setRefreshing(false);
+  };
 
   const type = "Novorapid";
   const units = "2";
@@ -14,42 +30,41 @@ const Insulin = () => {
   return (
     <SafeAreaView className="flex-1 bg-black">
       <FlatList
-        data={[]}
-        keyExtractor={(item) => item.$id}
-        renderItem={({ item }) => (
-          <Text>{item.$id}</Text>
+       data={posts}
+       keyExtractor={(item) => item.$id}
+       renderItem={({ item }) => (
+        <View className="bg-secondary-200 mt-2 mb-2 rounded-xl min-h-[62px] p-3">
+        <InfoBox
+          type={item.type == 1 ? "Novorapid" : "Levemir"}
+          units={item.units}
+          date={item.date}
+          containerStyles="mt-0"
+          titleStyles="text-lg"
+        />
+        
+      </View>
         )}
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
             <View className="flex flex-row justify-between items-start mb-6">
               <View>
-                <Text className="font-medium text-sm text-gray-100">
+                <Text className="font-medium text-xl text-gray-100">
                   {user?.username}
                 </Text>
-                <Text className="text-2xl font-semibold text-white">
+                <Text className="text-2xl font-semibold text-secondary">
                   Here are your most recent insulin insertions
                 </Text>
               </View>
             </View>
-            <View>
-            <View className="bg-secondary-200 mt-2 mb-2 rounded-xl min-h-[62px] p-3">
-              <InfoBox
-                type={type}
-                units={units}
-                date={date}
-                containerStyles="mt-0"
-                titleStyles="text-lg"
-              />
-              
-            </View>
-            
-           
-            </View>
+         
           </View>
         )}
         ListEmptyComponent={() => (
           <EmptyState title="No insertions created yet" />
         )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       />
     </SafeAreaView>
   );
