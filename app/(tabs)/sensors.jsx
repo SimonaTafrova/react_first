bbc0fc1123a71aac5aec23c7f68a2b12c2eef818
Simona Tafrova <simonatafrova@gmail.com>
2024-08-30 22:53:09 +0300

    Started creating sensors.jsx logic

--- app/(tabs)/sensors.jsx
+++ app/(tabs)/sensors.jsx
@@ -1,18 +1,25 @@
 import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { React, useState } from 'react'
 
 import { router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Image, TouchableOpacity } from "react-native";
 
-import { icons } from "../../constants";
+import { images } from "../../constants";
 import useAppwrite from "../../lib/useAppwrite";
 import { getUserPosts, signOut, getAllPrescriptions } from "../../lib/appwrite";
 import { useGlobalContext } from "../../context/GlobalProvider";
+import FormField from '../../components/FormField';
+import CustomButton from '../../components/CustomButton';
 
 
 const Sensors = () => {
   const { user} = useGlobalContext();
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [form, setform] = useState({
+    sensors: '',
+  })
+  const submit = () => {}
 
   
 
