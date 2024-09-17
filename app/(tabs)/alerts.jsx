8bbacabd8ebd69c01a47aa91f260c9a475b4418e
Simona Tafrova <simonatafrova@gmail.com>
2024-09-17 14:16:48 +0300

    started developing the layout of alerts.jsx

--- app/(tabs)/alerts.jsx
+++ app/(tabs)/alerts.jsx
@@ -1,12 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, RefreshControl, FlatList } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import EmptyState from '../../components/EmptyState';
+import InfoBox from '../../components/InfoBox';
+import useAppwrite from "../../lib/useAppwrite";
+import { useState } from 'react';
 import React from 'react'
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const Alerts = () => {
   return (
-    <View>
-      <Text>Alerts</Text>
-    </View>
-  )
+    <SafeAreaView className="flex-1 bg-black">
+      <FlatList
+       data={posts}
+       keyExtractor={(item) => item.$id}
+       renderItem={({ item }) => (
+        <View className="bg-secondary-200 mt-2 mb-2 rounded-xl min-h-[62px] p-3">
+        <InfoBox
+          textcontent={item.type == 1 ? `NovoRapid - ${item.units} units` : `Levemir - ${item.units} units`}
+          
+          date={formatTime(item.time)}
+          imagesource={images.insulin}
+          containerStyles="mt-0"
+          titleStyles="text-lg"
+        />
+        
+      </View>
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="flex flex-row justify-between items-start mb-6">
+              <View>
+                <Text className="font-medium text-xl text-gray-100">
+                  {user?.username}
+                </Text>
+                <Text className="text-2xl font-semibold text-secondary">
+                  Here are your 5 most recent insulin insertions
+                </Text>
+              </View>
+            </View>
+         
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState title="No insertions created yet" />
+        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
+    </SafeAreaView>
+  );
 }
 
 export default Alerts
